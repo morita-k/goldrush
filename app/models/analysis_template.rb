e7b2686aea62b31aa6db4a41b1add6463c80f45b
Aoino <aoiro.aoino@gmail.com>
2013-04-17 11:12:33 +0900

    インデントによるメール解析機能を追加

--- app/models/analysis_template.rb
+++ app/models/analysis_template.rb
@@ -15,38 +15,104 @@ class AnalysisTemplate < ActiveRecord::Base
   #
   #
   def AnalysisTemplate.analyze(analysis_template_id, import_mail, models)#biz_offer, business
-    AnalysisTemplateItem.find(:all, :conditions => ["deleted = 0 and analysis_template_id = ?", analysis_template_id]).each do |at_item|
-      option = at_item.ignore_flg == 1 ? Regexp::MULTILINE : nil
-      if import_mail.mail_body =~ Regexp.new(at_item.pattern, option)
+    
+    items = AnalysisTemplateItem.find(:all, :conditions => ["deleted = 0 and analysis_template_id = ?", analysis_template_id])
+    map = MailParser.new(import_mail).classification_by_indent
+    map.each do |k, v|
+      if k =~ Regexp.new(at_item.pattern)
         models.each do |model|
           next unless model.class.name == at_item.target_table_name.classify
 #            puts">>>>>>>>>>>>table_name  : "+model.class.name
 #            puts">>>>>>>>>>>>column_name : "+at_item.target_column_name
 #            puts">>>>>>>>>>>>value       : "+$1
-
           unless at_item.before_set_code.blank?
-            eval <<EOS
+            eval <<-EOS
               def AnalysisTemplate.before_set(model, at_item, str)
                 #{at_item.before_set_code}
               end
-EOS
+            EOS
             before_set(model, at_item, $1)
           end
 
-          model.send("#{at_item.target_column_name}=", $1)
+          model.send("#{at_item.target_column_name}=", v)
 
           unless at_item.after_set_code.blank?
-            eval <<EOS
+            eval <<-EOS
               def AnalysisTemplate.after_set(model, at_item, str)
                 #{at_item.after_set_code}
               end
-EOS
+            EOS
             after_set(model, at_item, $1)
           end
           
         end # models.each
-      end # each |at_item|
+      end
     end
+    
+#     AnalysisTemplateItem.find(:all, :conditions => ["deleted = 0 and analysis_template_id = ?", analysis_template_id]).each do |at_item|
+#       option = at_item.ignore_flg == 1 ? Regexp::MULTILINE : nil
+#       if import_mail.mail_body =~ Regexp.new(at_item.pattern, option)
+#         models.each do |model|
+#           next unless model.class.name == at_item.target_table_name.classify
+# #            puts">>>>>>>>>>>>table_name  : "+model.class.name
+# #            puts">>>>>>>>>>>>column_name : "+at_item.target_column_name
+# #            puts">>>>>>>>>>>>value       : "+$1
+#           unless at_item.before_set_code.blank?
+#             eval <<EOS
+#               def AnalysisTemplate.before_set(model, at_item, str)
+#                 #{at_item.before_set_code}
+#               end
+# EOS
+#             before_set(model, at_item, $1)
+#           end
+
+#           model.send("#{at_item.target_column_name}=", $1)
+
+#           unless at_item.after_set_code.blank?
+#             eval <<EOS
+#               def AnalysisTemplate.after_set(model, at_item, str)
+#                 #{at_item.after_set_code}
+#               end
+# EOS
+#             after_set(model, at_item, $1)
+#           end
+          
+#         end # models.each
+#       end # each |at_item|
+#     end
+  end
+
+end
+
+class AnalysisTemplate::MailParser
+
+  def initialize(here_document)
+    @mail_body_array = here_document.split(/\n/)
+  end
+  
+  def classification_by_indent
+    working_conditions = []
+    str = ""
+    @mail_body_array.each {|s|
+      s_head = s[0, 1]
+      if /\t/ =~ s_head || /\s/ =~ s_head || /　/ =~ s_head
+        str = str + s
+      else
+        working_conditions.push(str)
+        str = s
+      end
+    }
+    working_conditions.push(str) # ループで漏れる末端要素の追加
+    working_conditions.reject!{ |t| t == ""}
+    
+    # Refactoring and Hashing
+    hash = Hash.new
+    working_conditions.each { |c|
+      conditions_tag = c.split(/　|\s|:|：/)
+      conditions_tag.reject!{ |t| t == "" || t == "　" || t == " " || t == ":" || t == "："}
+      hash.store(conditions_tag.shift, conditions_tag.join("\n").lstrip)
+    }
+    hash
   end
 
 end
