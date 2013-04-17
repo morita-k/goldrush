c3f43946a8c5195fdd71201010e69d767a64d39b
Aoino <aoiro.aoino@gmail.com>
2013-04-17 11:42:55 +0900

    メール解析機能の細かい修正

--- app/models/analysis_template.rb
+++ app/models/analysis_template.rb
@@ -8,81 +8,53 @@ class AnalysisTemplate < ActiveRecord::Base
   belongs_to :bp_pic
   has_many :analysis_template_items, :conditions => ["analysis_template_items.deleted = 0"]
   
-  # メール解析処理のメイン
-  # 考え方
-  # AnalysisTemplateItemより、対象のテンプレートを取得
-  # メールのbodyに対して逐次正規表現マッチを行う
   #
+  # メール解析処理のメイン
+  # 考え方:
+  #   AnalysisTemplateItemより、対象のテンプレートを取得
+  #   各項目をインデントによって分類しハッシュを生成、キーに対して正規表現マッチを行う。
   #
   def AnalysisTemplate.analyze(analysis_template_id, import_mail, models)#biz_offer, business
     
     items = AnalysisTemplateItem.find(:all, :conditions => ["deleted = 0 and analysis_template_id = ?", analysis_template_id])
-    map = MailParser.new(import_mail).classification_by_indent
-    map.each do |k, v|
-      if k =~ Regexp.new(at_item.pattern)
-        models.each do |model|
-          next unless model.class.name == at_item.target_table_name.classify
-#            puts">>>>>>>>>>>>table_name  : "+model.class.name
-#            puts">>>>>>>>>>>>column_name : "+at_item.target_column_name
-#            puts">>>>>>>>>>>>value       : "+$1
-          unless at_item.before_set_code.blank?
-            eval <<-EOS
-              def AnalysisTemplate.before_set(model, at_item, str)
-                #{at_item.before_set_code}
-              end
-            EOS
-            before_set(model, at_item, $1)
-          end
-
-          model.send("#{at_item.target_column_name}=", v)
-
-          unless at_item.after_set_code.blank?
-            eval <<-EOS
-              def AnalysisTemplate.after_set(model, at_item, str)
-                #{at_item.after_set_code}
-              end
-            EOS
-            after_set(model, at_item, $1)
-          end
-          
-        end # models.each
-      end
-    end
+    map = MailParser.new(import_mail.mail_body).classification_by_indent
     
-#     AnalysisTemplateItem.find(:all, :conditions => ["deleted = 0 and analysis_template_id = ?", analysis_template_id]).each do |at_item|
-#       option = at_item.ignore_flg == 1 ? Regexp::MULTILINE : nil
-#       if import_mail.mail_body =~ Regexp.new(at_item.pattern, option)
-#         models.each do |model|
-#           next unless model.class.name == at_item.target_table_name.classify
-# #            puts">>>>>>>>>>>>table_name  : "+model.class.name
-# #            puts">>>>>>>>>>>>column_name : "+at_item.target_column_name
-# #            puts">>>>>>>>>>>>value       : "+$1
-#           unless at_item.before_set_code.blank?
-#             eval <<EOS
-#               def AnalysisTemplate.before_set(model, at_item, str)
-#                 #{at_item.before_set_code}
-#               end
-# EOS
-#             before_set(model, at_item, $1)
-#           end
+    map.each { |k, v|
+      items.each {|item|
+        if k =~ Regexp.new(item.pattern)
+          models.each do |model|
+            next unless model.class.name == item.target_table_name.classify
+  #            puts">>>>>>>>>>>>table_name  : "+model.class.name
+  #            puts">>>>>>>>>>>>column_name : "+at_item.target_column_name
+  #            puts">>>>>>>>>>>>value       : "+$1
+            unless item.before_set_code.blank?
+              eval <<-EOS
+                def AnalysisTemplate.before_set(model, item, str)
+                  #{at_item.before_set_code}
+                end
+              EOS
+              before_set(model, item, $1)
+            end
 
-#           model.send("#{at_item.target_column_name}=", $1)
+            model.send("#{item.target_column_name}=", v)
 
-#           unless at_item.after_set_code.blank?
-#             eval <<EOS
-#               def AnalysisTemplate.after_set(model, at_item, str)
-#                 #{at_item.after_set_code}
-#               end
-# EOS
-#             after_set(model, at_item, $1)
-#           end
-          
-#         end # models.each
-#       end # each |at_item|
-#     end
+            unless item.after_set_code.blank?
+              eval <<-EOS
+                def AnalysisTemplate.after_set(model, item, str)
+                  #{at_item.after_set_code}
+                end
+              EOS
+              after_set(model, item, $1)
+            end
+            
+          end # models.each
+        end
+      }
+    }
   end
 
 end
+
 
 class AnalysisTemplate::MailParser
 
