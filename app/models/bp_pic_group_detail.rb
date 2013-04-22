274bb34219293538b7ed60c8a6871f5698c7a02d
nakano-m <nakano-m@applicative.co.jp>
2013-04-22 17:34:04 +0900

    typo修正

--- app/models/bp_pic_group_detail.rb
+++ app/models/bp_pic_group_detail.rb
@@ -1,8 +1,8 @@
 class BpPicGroupDetail < ActiveRecord::Base
   belongs_to :bp_pic
-  attr_accessible :bp_pic_group_id, :bp_pic_id, :id, :memo, :owner_id, :lock_version
+  attr_accessible :bp_pic_group_id, :bp_pic_id, :id, :memo, :owner_id, :suspended, :lock_version
   
-  def supended?
+  def suspended?
     suspended == 1
   end
 end
