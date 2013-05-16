98fd1b24a8a0ae9d0e8940fc9d2632e5a782a8c7
Tatsunori TAMAKI <github@qtamaki.com>
2013-05-16 16:28:01 +0900

    エラー時にフィールドがピンクになるように

--- config/application.rb
+++ config/application.rb
@@ -61,4 +61,5 @@ module GoldRush
     config.assets.version = '1.0'
 
   end
+  ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| "<span class=\"field_with_errors\">#{html_tag}</span>".html_safe }
 end
