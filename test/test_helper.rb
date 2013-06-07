33ea9f72098549416f36e1bb874e770c7afb3bb6
Tatsunori TAMAKI <github@qtamaki.com>
2013-06-07 13:32:18 +0900

    Jenkinsようにsimplecov追加 テストケースのエラー修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
