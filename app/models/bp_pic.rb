6e5038070054218ca78d2721742bf3489071c057
keiteio <mss.ymns@gmail.com>
2013-07-26 18:22:43 +0900

    [チケット#298] [取引先]送信停止で検索できるようにする

--- app/models/bp_pic.rb
+++ app/models/bp_pic.rb
@@ -5,7 +5,10 @@ class BpPic < ActiveRecord::Base
   has_one :delivery_mail_target
   has_many :businesses
   has_many :bp_members
-   
+  
+  # TODO: 複数形で指定すると、取引先一覧画面のpaginateでエラーが出るため単数形で記述
+  has_many :bp_pic_group_detail, :conditions => "bp_pic_group_details.deleted = 0"
+  
   validates_presence_of :bp_pic_name, :bp_pic_short_name, :bp_pic_name_kana, :email1
 #  validates_uniqueness_of :bp_pic_name, :case_sensitive => false, :scope => :business_partner_id
 #  validates_uniqueness_of :bp_pic_name_kana, :case_sensitive => false, :scope => :business_partner_id
