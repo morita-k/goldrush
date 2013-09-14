1db4a6db08f54cfeb54eb280cd80f5862b0c2859
Tatsunori TAMAKI <qtamaki@gmail.com>
2013-09-14 12:25:48 +0900

    変数名変更

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -90,7 +90,7 @@ class ApiController < ApplicationController
     render :json => BaseMonth.where("deleted = 0 and start_date = ?", params[:start_date]).first.monthly_workings.map{|m|
       {
         :employee_name => m.user.employee.employee_name,
-        :workwheets => m.daily_workings.map{|w|
+        :working_list => m.daily_workings.map{|w|
           {
             :working_date => w.working_date,
             :working_type => w.working_type_short_name,
