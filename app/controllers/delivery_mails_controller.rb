0a3af1490ba8a1840fa833afd0d7c89b61988786
Tatsunori TAMAKI <github@qtamaki.com>
2014-06-02 13:07:26 +0900

    あいさつメール作成をメール作成に変更

--- app/controllers/delivery_mails_controller.rb
+++ app/controllers/delivery_mails_controller.rb
@@ -291,7 +291,7 @@ EOS
     @delivery_mail = DeliveryMail.new
     #@delivery_mail.bp_pic_group_id = params[:id]
     unless sales_pic = BpPic.find(params[:bp_pic_ids][0]).sales_pic
-      raise ValidationAbort.new("Contact mail method is wants sales_pic id.")
+      sales_pic = current_user
     end
     @delivery_mail.content = ""
     if t = sales_pic.contact_mail_template
