f3139855736ef5f22f4a4a17faa9638114e8f92b
Tatsunori TAMAKI <github@qtamaki.com>
2014-06-30 18:16:59 +0900

    リンク切れ

--- app/helpers/import_mail_helper.rb
+++ app/helpers/import_mail_helper.rb
@@ -129,7 +129,8 @@ module ImportMailHelper
   end
 
   def show_stars(import_mail)
-     (import_mail.bp_pic_id.blank? ? raw("☆☆") : star_links(import_mail.business_partner, import_mail.business_partner.business_partner_name) + star_links(import_mail.bp_pic, import_mail.bp_pic.bp_pic_name)) + star_links(import_mail, import_mail.mail_subject) + " " + (back_to_link raw('<i class="glyphicon glyphicon-log-in"></i>'), :action => :show, :id => import_mail)
+    dummy_star = raw("☆☆")
+     (import_mail.bp_pic_id.blank? ? dummy_star : star_links(import_mail.business_partner, import_mail.business_partner.business_partner_name) + star_links(import_mail.bp_pic, import_mail.bp_pic.bp_pic_name)) + star_links(import_mail, import_mail.mail_subject) + " " + (back_to_link raw('<i class="glyphicon glyphicon-log-in"></i>'), :controller => :import_mail, :action => :show, :id => import_mail)
   end
 end
 
