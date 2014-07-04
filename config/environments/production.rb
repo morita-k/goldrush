9bc4b5a7989de550cb73feb45973ccad6aaecb74
GoldRush <goldrush@applicative.jp>
2014-07-04 12:24:45 +0900

    DKIMの設定をproduction.rbに移動

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,3 +76,13 @@ ActionMailer::Base.smtp_settings = {
   :domain             => 'gr.applicative.jp',
 }
 
+#Dkim::domain      = 'applicative.co.jp'
+Dkim::domain      = 'gr.applicative.jp'
+Dkim::identity    = 'error@gr.applicative.jp'
+Dkim::selector    = 'default'
+Dkim::private_key = open('config/applicative.co.jp.dkim.key').read
+ActionMailer::Base.register_interceptor(Dkim::Interceptor)
+
+#Dkim::signable_headers  = Dkim::DefaultHeaders + %w{Return-Path}
+
+
