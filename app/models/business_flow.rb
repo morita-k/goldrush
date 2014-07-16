2aac84b33c57b9bb8b8ec7bfe3d630ca58a39375
Tatsunori TAMAKI <github@qtamaki.com>
2014-07-16 17:49:49 +0900

    コメントアウト処理を削除

--- app/models/business_flow.rb
+++ app/models/business_flow.rb
@@ -30,7 +30,7 @@ module BusinessFlow
   
   # actionを実行する。:actionのレコードにeventが設定されていた場合evnt実行
   def do_action(action, status_type = detect_default_status_type)
-    if a = @actions_map[status_type].detect{|a| a.action == action}
+    if a = @actions_map[status_type].detect{|x| x.action == action}
       if a.event
         send("#{status_type}=", a.event.call(a))
       else
@@ -49,82 +49,5 @@ module BusinessFlow
        action.from != send(status_type).to_sym
      end
   end
-
-=begin
-  # ステータスとアクションの定義
-  def business_status_types
-    [:offered, :working, :finished, :other_failure, :lost_failure, :natural_lost]
-  end
-  def biz_offer_status_types
-    [:open,:approached, :working, :finished,:other_failure,:lost_failure,:natural_lost,:other_success]
-  end
-  def approach_status_types
-    [:approaching,:adjust,:result_waiting, :working, :finished, :approach_failure,:interview_failure,:other_failure,:lost_failure,:natural_lost,:other_success]
-  end
-  def human_resource_status_types
-    [:sales,:approached,:working,:unknown]
-  end
-  def contract_status_types
-    [:open,:contract,:finished]
-  end
-  def upper_contract_status_types
-    [:waiting_order,:proc_acceptance,:contracted,:finished,:abort,:confirming,:closed]
-  end
-  def down_contract_status_types
-    [:waiting_offer,:proc_order,:waiting_acceptance,:contracted,:finished,:abort,:confirming,:closed]
-  end
-  def actions
-    [:accept_order,:send_accept,:end_term,:abort_term,:prepare_next,:accept_continue_term,:accept_end_term,:notify_order,:send_order,:accept_accept,:send_continue_term,:send_end_term]
-  end
-
-  # アクションによるステータスの変化を定義
-  def do_business_status_type_action(action)
-    {:end_term => :finished, :abort_term => :finished, :accept_end_term => :finished}[action]
-  end
-  def do_biz_offer_status_type_action(action)
-    {:end_term => :finished, :abort_term => :finished, :accept_end_term => :finished}[action]
-  end
-  def do_approach_status_type_action(action)
-    {:end_term => :finished, :abort_term => :finished, :accept_end_term => :finished}[action]
-  end
-  def do_human_resource_status_type_action(action)
-    {:end_term => :finished, :abort_term => :finished, :accept_end_term => :finished}[action]
-  end
-
-  def xexe(xss)
-    status_to_actions = {}
-    action_to_status = {}
-    xss.each do |xs|
-      status_to_actions[xs[0]] ||= []
-      status_to_actions[xs[0]] << xs[1]
-      action_to_status[xs[1]] = xs[2]
-    end
-    return [status_to_actions, action_to_status]
-  end
-
-  def xexe_business_status_type_actions
-    xexe([
-    
-    ])
-  end
-
-  #----------------------------------------------------------
-
-  def init_status
-    @statuses = {
-      :business_status_type => :working,
-      :biz_offer_status_type => :working,
-      :approach_status_type => :working,
-      :human_resource_status_type => :working,
-      :contract_status_type => :open,
-      :upper_contract_status_type => :waiting_order,
-      :down_contract_status_type => :waiting_offer,
-    }
-  end
-
-  def do_action(action)
-  end
-  
-=end
 end
 
