96e093247a59cd346301da5081e7d76c251a8f71
nakano-m <nakano-m@applicative.co.jp>
2014-10-08 13:58:05 +0900

    登録時、owner別に同一のデータを作る事ができないバグを修正

--- app/models/bp_pic.rb
+++ app/models/bp_pic.rb
@@ -16,7 +16,7 @@ class BpPic < ActiveRecord::Base
   validates_presence_of :bp_pic_name, :bp_pic_short_name, :bp_pic_name_kana, :email1
 #  validates_uniqueness_of :bp_pic_name, :case_sensitive => false, :scope => :business_partner_id
 #  validates_uniqueness_of :bp_pic_name_kana, :case_sensitive => false, :scope => :business_partner_id
-  validates_uniqueness_of :email1, :case_sensitive => false, :scope => [:deleted, :deleted_at]
+  validates_uniqueness_of :email1, :case_sensitive => false, :scope => [:owner_id, :deleted, :deleted_at]
   
   NONDELIVERY_LIMIT=3
   BOUNCE_MAIL_REASON_ERROR = [:hostunknown, :userunknown]
