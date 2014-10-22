79f67600090c9d052abb704b8710b69ae82591ba
nakano-m <nakano-m@applicative.co.jp>
2014-10-22 10:47:14 +0900

    復号化に失敗した場合空を返すよう修正

--- lib/smtp_password_encryptor.rb
+++ lib/smtp_password_encryptor.rb
@@ -1,11 +1,19 @@
 # -*- encoding: utf-8 -*-
 module SmtpPasswordEncryptor
+  # 暗号化
+  # パスワードが空でも暗号化したパスワードを返す
   def self.encrypt(password)
     self._new_encryptor.encrypt_and_sign(password)
   end
 
+  # 復号化
+  # 復号化に失敗した場合、空文字を返す
   def self.decrypt(encrypted_password)
-    self._new_encryptor.decrypt_and_verify(encrypted_password)
+    begin
+      self._new_encryptor.decrypt_and_verify(encrypted_password)
+    rescue
+      ''
+    end
   end
 
 private
