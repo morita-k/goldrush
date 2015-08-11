3904f2a53d4ec946a532e4162831d6c608496817
GoldRush <goldrush@applicative.jp>
2015-08-11 10:13:00 +0900

    select count(*)が走っていたので元に戻す

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -322,13 +322,7 @@ EOS
   end
 
   def paginate_far(scope, options = {}, &block)
-    # 件数が多いので、総ページ数は高々現在ページ+1とする
-    total_pages = 1
-    if scope.num_pages > 1
-      total_pages = scope.current_page + (scope.current_page < scope.num_pages ? 1 : 0)
-    end
-    paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => scope.current_page, :total_pages => total_pages, :per_page => scope.limit_value, :param_name => Kaminari.config.param_name, :remote => false)
-    paginator.to_s
+    paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => scope.current_page, :total_pages => scope.current_page + 1, :per_page => scope.limit_value, :param_name => Kaminari.config.param_name, :remote => false)
   end
 
   # put_paginatesで使う関数
